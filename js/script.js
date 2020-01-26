ca36b7964de739c9067b9ea8b8103713c19dd2f9
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-26 13:49:22 +0100

    Added a few js functions

--- js/script.js
+++ js/script.js
@@ -1,2 +1,40 @@
-// hello world
+// code
 console.log('hello world');
+
+// function
+// generate random numbers between min and max
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// check if an item is stored inside an array
+function scanArray(array, item) {
+  var i = 0;
+  while (i < array.length) {
+    if (array[i] === item) {
+      return true;
+    }
+    i++;
+  }
+  return false;
+}
+
+// check that a number is between min and max
+function checkMinMax(min, max, number) {
+  if (number >= min && number <= max) {
+    return true;
+  }
+  return false;
+}
+
+// check if two arrays are exactly the same
+function compareArray(array1, array2) {
+  for (var i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
